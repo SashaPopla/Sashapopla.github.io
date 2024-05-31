9827daddad94c1731c2314d663d819b37cfed511
Sasha <saspoplavskiy563@gmail.com>
2024-05-31 09:32:24 +0300

    new script for button

--- js/script.js
+++ js/script.js
@@ -1,109 +1,118 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  function conectExelSheets(url){
+  function conectExelSheets(url) {
     axios
-    .get(url)
-    .then((response) => {
-      let csvData = response.data;
-      let jsonData = Papa.parse(csvData, { header: true }).data;
+      .get(url)
+      .then((response) => {
+        let csvData = response.data;
+        let jsonData = Papa.parse(csvData, { header: true }).data;
 
-      for (let key in jsonData) {
-        let name = jsonData[key]["Людина яка проходила"];
+        for (let key in jsonData) {
+          let name = jsonData[key]["Людина яка проходила"];
 
-        if (name == "") {
-          let temp = jsonData[--key];
+          if (name == "") {
+            let temp = jsonData[--key];
 
-          let data = [
-            { name: "Очаровуючий маніпулятор", value: +temp['Очаровуючий маніпулятор'] },
-            { name: "Творчий мрійник", value: +temp['Творчий мрійник'] },
-            { name: "Надреагуючий ентузіаст", value: +temp['Надреагуючий ентузіаст'] },
-            { name: "Відповідальний роботоголік", value: +temp['Відповідальний роботоголік'] },
-            { name: "Блискучий скептик", value: +temp['Блискучий скептик'] },
-            { name: "Грайливий опозиціонер", value: +temp['Грайливий опозиціонер'] },
-          ];
-
-          console.log(data);
-
-          function createDiagram(){
-            let colors = [
-              "#1f77b4",
-              "#ff7f0e",
-              "#2ca02c",
-              "#d62728",
-              "#9467bd",
-              "#8c564b",
+            let data = [
+              {
+                name: "Очаровуючий маніпулятор",
+                value: +temp["Очаровуючий маніпулятор"],
+              },
+              { name: "Творчий мрійник", value: +temp["Творчий мрійник"] },
+              {
+                name: "Надреагуючий ентузіаст",
+                value: +temp["Надреагуючий ентузіаст"],
+              },
+              {
+                name: "Відповідальний роботоголік",
+                value: +temp["Відповідальний роботоголік"],
+              },
+              { name: "Блискучий скептик", value: +temp["Блискучий скептик"] },
+              {
+                name: "Грайливий опозиціонер",
+                value: +temp["Грайливий опозиціонер"],
+              },
             ];
-          
-            // Визначення ширини, висоти та відступів
-            let width = 800;
-            let height = 500;
-            let margin = { top: 60, right: 30, bottom: 2000, left: 180 };
-          
-            // Створення SVG елементу
-            let svg = d3
-              .select("body")
-              .append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-          
-            // Створення шкали X
-            let x = d3
-              .scaleBand()
-              .range([0, width])
-              .domain(
-                data.map(function (d) {
-                  return d.name;
-                })
-              )
-              .padding(0.2);
-          
-            svg
-              .append("g")
-              .attr("transform", "translate(0," + height + ")")
-              .call(d3.axisBottom(x))
-              .selectAll("text")
-              .attr("transform", "translate(-10,0)rotate(-45)")
-              .style("text-anchor", "end")
-              .style("font-size", "18px");
-          
-            // Створення шкали Y
-            let y = d3.scaleLinear().domain([0, 10]).range([height, 0]);
-          
-            svg.append("g").call(d3.axisLeft(y));
-          
-            // Створення барів для гістограми
-            svg
-              .selectAll("mybar")
-              .data(data)
-              .enter()
-              .append("rect")
-              .attr("x", function (d) {
-                return x(d.name);
-              })
-              .attr("y", function (d) {
-                return y(d.value);
-              })
-              .attr("width", x.bandwidth())
-              .attr("height", function (d) {
-                return height - y(d.value);
-              })
-              .attr("fill", function (d, i) {
-                return colors[i % colors.length];
-              });
-          }
 
-          createDiagram();
-          break;
+            console.log(data);
+
+            function createDiagram() {
+              let colors = [
+                "#1f77b4",
+                "#ff7f0e",
+                "#2ca02c",
+                "#d62728",
+                "#9467bd",
+                "#8c564b",
+              ];
+
+              let width = 800;
+              let height = 500;
+              let margin = { top: 60, right: 30, bottom: 2000, left: 180 };
+
+              let svg = d3
+                .select("body")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr(
+                  "transform",
+                  "translate(" + margin.left + "," + margin.top + ")"
+                );
+
+              let x = d3
+                .scaleBand()
+                .range([0, width])
+                .domain(
+                  data.map(function (d) {
+                    return d.name;
+                  })
+                )
+                .padding(0.2);
+
+              svg
+                .append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end")
+                .style("font-size", "18px");
+
+              let y = d3.scaleLinear().domain([0, 10]).range([height, 0]);
+
+              svg.append("g").call(d3.axisLeft(y));
+
+              svg
+                .selectAll("mybar")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("x", function (d) {
+                  return x(d.name);
+                })
+                .attr("y", function (d) {
+                  return y(d.value);
+                })
+                .attr("width", x.bandwidth())
+                .attr("height", function (d) {
+                  return height - y(d.value);
+                })
+                .attr("fill", function (d, i) {
+                  return colors[i % colors.length];
+                });
+            }
+
+            createDiagram();
+            break;
+          }
         }
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   const url =
